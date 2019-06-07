a2aacf73ecc4ea9e207b7bffc5998d0b406832b9
Karun Kesavadas <karun.kesavadas@gmail.com>
2019-06-07 03:18:40 -0400

    Implemented Current Time API & Username/Password Validation

--- webapp/src/main/java/com/neu/ccwebapp/service/UserServiceImpl.java
+++ webapp/src/main/java/com/neu/ccwebapp/service/UserServiceImpl.java
@@ -3,17 +3,44 @@ package com.neu.ccwebapp.service;
 import com.neu.ccwebapp.domain.User;
 import com.neu.ccwebapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class UserServiceImpl implements UserService
+public class UserServiceImpl implements UserService, UserDetailsService
 {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public void registerUser(User user)
     {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException
+    {
+        Optional<User> user = userRepository.findById(userName);
+        if(user.isEmpty())
+        {
+            throw new UsernameNotFoundException("No user found with the username : "+userName);
+        }
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new org.springframework.security.core.userdetails.User(user.get().getUsername(), user.get().getPassword(), authorities);
     }
 }
--- webapp/src/main/java/com/neu/ccwebapp/web/AppController.java
+++ webapp/src/main/java/com/neu/ccwebapp/web/AppController.java
@@ -1,12 +1,15 @@
 package com.neu.ccwebapp.web;
 
+import com.neu.ccwebapp.domain.CurrentTime;
 import com.neu.ccwebapp.domain.User;
 import com.neu.ccwebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class AppController
@@ -14,13 +17,15 @@ public class AppController
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     @PostMapping("/user/register")
-    public void registerUser(@RequestBody User user)
+    public void registerUser(@Valid @RequestBody User user)
     {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.registerUser(user);
+    }
+
+    @GetMapping("/")
+    public CurrentTime getCurrentTime()
+    {
+        return new CurrentTime();
     }
 }
