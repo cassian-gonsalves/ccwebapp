e81204900da5998c72a829a231b7eb9e7887cdf5
DhanashriAPalodkar <palodkar.d@husky.neu.edu>
2019-06-14 17:46:57 -0400

    Added image APIs

---
+++ webapp/src/main/java/com/neu/ccwebapp/exceptions/FileStorageException.java
@@ -0,0 +1,12 @@
+package com.neu.ccwebapp.exceptions;
+
+public class FileStorageException extends RuntimeException
+{
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- webapp/src/main/java/com/neu/ccwebapp/service/ImageService.java
+++ webapp/src/main/java/com/neu/ccwebapp/service/ImageService.java
@@ -2,20 +2,23 @@ package com.neu.ccwebapp.service;
 
 import com.neu.ccwebapp.domain.Image;
 import com.neu.ccwebapp.exceptions.BookNotFoundException;
+import com.neu.ccwebapp.exceptions.ImageExistsException;
 import com.neu.ccwebapp.exceptions.ImageNotFoundException;
+import com.neu.ccwebapp.exceptions.InvalidFileException;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 public interface ImageService  {
 
 
-    public void addBookImage(UUID uuid, Image image) throws BookNotFoundException;
+    public Image addBookImage(UUID uuid, MultipartFile image) throws BookNotFoundException, ImageExistsException, InvalidFileException;
 
-    public Image getImageById(UUID idBook,UUID idImage) throws ImageNotFoundException;
+    public Image getImageById(UUID idBook,UUID idImage) throws ImageNotFoundException, BookNotFoundException;
 
-    public void updateImage(UUID idBook,UUID idImage, Image image) throws ImageNotFoundException;
+    public void updateImage(UUID idBook,UUID idImage, MultipartFile image) throws ImageNotFoundException, BookNotFoundException, InvalidFileException;
 
-    public void deleteImage(UUID idBook,UUID idImage) throws ImageNotFoundException;
+    public void deleteImage(UUID idBook,UUID idImage) throws ImageNotFoundException, BookNotFoundException;
 
 
 
