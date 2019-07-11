3a136f87740b7645d6e275c284b3cd48fecc63b1
Karun Kesavadas <karun.kesavadas@gmail.com>
2019-07-11 00:26:06 -0400

    Added IAM Policies and Roles for CI/CD Pipeline

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-iam-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-iam-stack.sh
@@ -10,20 +10,25 @@ if [ -z "$USER_NAME" ]; then
 	echo "\e[31m\e[1m ERROR : CIRCLECI USER'S USERNAME WAS NOT PROVIDED!\e[0m"
 	exit 1
 fi
-BUCKET_NAME=$3
-if [ -z "$BUCKET_NAME" ]; then
+CODEDEPLOY_BUCKET_NAME=$3
+if [ -z "$CODEDEPLOY_BUCKET_NAME" ]; then
 	echo "\e[31m\e[1m ERROR : S3 BUCKET NAME FOR CODEDEPLOY WAS NOT PROVIDED!\e[0m"
 	exit 1
 fi
-APPLICATION_NAME=$4	
+IMAGE_UPLOAD_BUCKET_NAME=$4
+if [ -z "$IMAGE_UPLOAD_BUCKET_NAME" ]; then
+	echo "\e[31m\e[1m ERROR : S3 BUCKET NAME FOR IMAGE UPLOAD WAS NOT PROVIDED!\e[0m"
+	exit 1
+fi
+APPLICATION_NAME=$5	
 if [ -z "$APPLICATION_NAME" ]; then
 	echo "\e[31m\e[1m ERROR : CODEDEPLOY APPLICATION NAME WAS NOT PROVIDED!\e[0m"
 	exit 1
 fi
 
 echo "\e[32m\e[1mCREATING IAM STACK!!\e[0m"
-stackCreation=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-iam.json --capabilities CAPABILITY_IAM --parameters \
-ParameterKey=CircleCIUser,ParameterValue=$USER_NAME ParameterKey=ApplicationName,ParameterValue=$APPLICATION_NAME ParameterKey=CodeDeployS3Bucket,ParameterValue=$BUCKET_NAME)
+stackCreation=$(aws cloudformation create-stack --stack-name $STACK_NAME --template-body file://csye6225-cf-iam.json --capabilities CAPABILITY_NAMED_IAM --parameters \
+ParameterKey=CircleCIUser,ParameterValue=$USER_NAME ParameterKey=ApplicationName,ParameterValue=$APPLICATION_NAME ParameterKey=CodeDeployS3Bucket,ParameterValue=$CODEDEPLOY_BUCKET_NAME ParameterKey=ImageUploadS3Bucket,ParameterValue=$IMAGE_UPLOAD_BUCKET_NAME)
 if [ $? -eq 0 ]; then
 	stackCompletion=$(aws cloudformation wait stack-create-complete --stack-name $STACK_NAME)
 	if [ $? -eq 0 ]; then
