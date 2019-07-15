f39d0a673e4588e995887722e2d474025dcbb95b
Cassian Gonsalves <gonsalves.c@husky.neu.edu>
2019-07-15 18:38:05 -0400

    REverted changes

--- webapp/src/main/java/com/neu/ccwebapp/web/AppController.java
+++ webapp/src/main/java/com/neu/ccwebapp/web/AppController.java
@@ -48,7 +48,7 @@ public class AppController {
         }
     }
 
-    @GetMapping("/cassian")
+    @GetMapping("/book")
     public List<Book> getBook()
     {
         List<Book> books = new ArrayList<>();
